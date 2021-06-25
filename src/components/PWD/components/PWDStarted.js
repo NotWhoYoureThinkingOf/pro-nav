87404e4a000980079ee0fffb08bd705ac7cbb535
NotWhoYoureThinkingOf <acurtis1286@gmail.com>
2021-06-25 17:33:49 -0400

    added some markup for prowash

--- src/components/PWD/components/PWDStarted.js
+++ src/components/PWD/components/PWDStarted.js
@@ -4,7 +4,12 @@ import './PWDStarted.css'
 const PWDStarted = () => {
     return (
         <div className="pwdStarted">
-
+            <div className="pwdStarted__washType">
+                <div className="pwdStarted__platform"></div>
+            </div>
+            <div className="pwdStarted__process"></div>
+            <div className="pwdStarted__time"></div>
+            <div className="pwdStarted__cancel"></div>
         </div>
     )
 }
